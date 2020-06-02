27b6e99d2ac17b9db09fed0c8b8659d1adbe8df1
Georgette Class-Peters <georgycp@udel.edu>
2020-06-02 17:59:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,4 +10,9 @@
 # NO, NOT SINCE 1938!
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+# I LOVE YOU TOO PUMPKIN!''
+ def speak_to_grandma
+  if "I LOVE YOU GRANDMA!"
+  return"I LOVE YOU TOO PUMPKIN!"
+end
+end
